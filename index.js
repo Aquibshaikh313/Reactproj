b245e82ce7b508901cad84bd8a8c4ecee4dfdba4
Aquibshaikh313 <105275742+Aquibshaikh313@users.noreply.github.com>
2023-01-26 16:14:20 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,17 +1,32 @@
-//For Add item to cart
-export function addCart(product)
-{
-    return{
-        type: "ADDITEM",
-        payload: product
-    }
-}
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+// import './index.css';
+import reportWebVitals from './reportWebVitals';
+import '../node_modules/bootstrap/dist/css/bootstrap.css';
+import '../node_modules/bootstrap-icons/font/bootstrap-icons.css';
+import '../node_modules/jquery/dist/jquery.js';
+import '../node_modules/bootstrap/dist/js/bootstrap.bundle.js';
 
-//For Delete item from Cart
-export function delCart(product)
-{
-    return{
-        type:"DELITEM",
-        payload: product
-    }
-}
+import { ShoppingIndex } from './SPA/ShoppingIndex';
+import { CookiesProvider } from 'react-cookie';
+
+
+import { Provider } from 'react-redux';
+import store from './redux/store';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+
+  <React.StrictMode>
+    <CookiesProvider>
+      <Provider store={store}>
+        <ShoppingIndex />
+      </Provider>
+    </CookiesProvider>
+
+
+  </React.StrictMode>
+);
+
+
+reportWebVitals();
